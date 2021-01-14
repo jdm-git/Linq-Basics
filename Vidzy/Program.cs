c3e3877be37afc684dd03f0d116f50e2c727f744
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-01-14 18:09:22 +0100

    Initial Commit

--- Vidzy/Program.cs
+++ Vidzy/Program.cs
@@ -10,59 +10,59 @@ namespace Vidzy
 
             var queryActionMovies = context.Videos.Where(m => m.Genre.Name == "Action");
 
-            //foreach (var movies in queryActionMovies)
-            //{
-            //    System.Console.WriteLine(movies.Name);
-            //}
-            //var queryGoldDramaMovies = context.Videos
-            //    .Where(m => m.Genre.Name == "Drama" && m.Classification == Classification.Gold)
-            //    .OrderBy(m => m.ReleaseDate);
+            foreach (var movies in queryActionMovies)
+            {
+                System.Console.WriteLine(movies.Name);
+            }
+            var queryGoldDramaMovies = context.Videos
+                .Where(m => m.Genre.Name == "Drama" && m.Classification == Classification.Gold)
+                .OrderBy(m => m.ReleaseDate);
 
 
-            //foreach (var movie in queryGoldDramaMovies)
-            //{
-            //    System.Console.WriteLine(movie.Name);
-            //}
+            foreach (var movie in queryGoldDramaMovies)
+            {
+                System.Console.WriteLine(movie.Name);
+            }
 
-            //var query = context.Videos.Select(v => new { MovieName = v.Name, Genre = v.Genre.Name });
+            var queryAnonymousObj = context.Videos.Select(v => new { MovieName = v.Name, Genre = v.Genre.Name });
 
-            //foreach (var movie in query)
-            //{
-            //    System.Console.WriteLine($"{movie.MovieName}\t {movie.Genre}");
-            //}
+            foreach (var movie in queryAnonymousObj)
+            {
+                System.Console.WriteLine($"{movie.MovieName}\t {movie.Genre}");
+            }
 
 
-            //var query = context.Videos
-            //    .GroupBy(m => m.Classification)
-            //    .Select(g => new 
-            //    { 
-            //        Classification = g.Key.ToString(),
-            //        Movies = g.OrderBy(v => v.Name)
-            //    });
+            var queryGroupingByClassification = context.Videos
+                .GroupBy(m => m.Classification)
+                .Select(g => new
+                {
+                    Classification = g.Key.ToString(),
+                    Movies = g.OrderBy(v => v.Name)
+                });
 
-            //foreach (var group in query)
-            //{
-            //    System.Console.WriteLine($"Classification: {group.Classification}");
+            foreach (var group in queryGroupingByClassification)
+            {
+                System.Console.WriteLine($"Classification: {group.Classification}");
 
-            //    foreach (var movie in group.Movies)
-            //    {
-            //        System.Console.WriteLine("\t" + movie.Name);
-            //    }
-            //}
+                foreach (var movie in group.Movies)
+                {
+                    System.Console.WriteLine("\t" + movie.Name);
+                }
+            }
 
-            //var query = context.Videos
-            //    .GroupBy(m => m.Classification)
-            //    .Select(g => new
-            //{
-            //    Name = g.Key.ToString(),
-            //    Count = g.Count()
-            //})
-            //    .OrderBy(g => g.Name);
+            var queryGroupAndCounting = context.Videos
+                .GroupBy(m => m.Classification)
+                .Select(g => new
+                {
+                    Name = g.Key.ToString(),
+                    Count = g.Count()
+                })
+                .OrderBy(g => g.Name);
 
-            //foreach (var classification in query)
-            //{
-            //    System.Console.WriteLine(classification.Name + classification.Count);
-            //}
+            foreach (var classification in queryGroupAndCounting)
+            {
+                System.Console.WriteLine(classification.Name + classification.Count);
+            }
 
             //var query = context.Genres
             //    .Select(g => new
